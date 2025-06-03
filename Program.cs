c99792d6f02bbd830e065fd8a42ad28c9554a489
Kim tran <thuyoanh21790@gmail.com>
2025-06-03 15:23:51 +0700

    Remove unsupported MapStaticAssets and WithStaticAssets calls

--- Program.cs
+++ Program.cs
@@ -29,12 +29,10 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
 
 
 app.Run();
